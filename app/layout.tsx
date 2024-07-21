1310e711ef985b3af68b95312f3f87456eabe997
Audyari Wiyono <audy123ari@gmail.com>
2024-07-21 21:24:03 +0700

    ara Menggunakan Typescript Di Project Next.Js

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <header>[header]</header>
+        <main>{children}</main>
+        <footer>[footer]</footer>
+      </body>
+    </html>
+  )
+}

f6e9cde6d6f0be76a1c95ad0be8a9331483008d6
Audyari Wiyono <audy123ari@gmail.com>
2024-07-21 20:56:31 +0700

    Cara Membuat Halaman Web

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,14 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Belajar NextJs</title>
+      </head>
+      <header>[header]</header>
+      <main>{children}</main>
+      <footer>[footer]</footer>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
+import { Fragment } from "react";
+
 export default function Page() {
-  return <h1>Tampilan Next Pertamaku</h1>;
+  return (
+    <Fragment>
+      <h1>Tampilan Next Pertamaku</h1>
+      <p>Ini adalah halaman pertama saya</p>
+    </Fragment>
+  );
 }
